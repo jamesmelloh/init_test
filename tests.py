f45bd7b1491bbe234ea2f05401c1c7fa58786336
jamesmelloh <james.melloh@sanjuansw.com>
2018-07-26 10:34:37 -0700

    tests.py changed

--- tests.py
+++ tests.py
@@ -1,28 +1,28 @@
 #tests.py
-# import sys
-# import os
-# try:
-#     print('JENKINS_WORKSPACE' + os.environ.get('JENKINS_HOME'))
-# except KeyError:
-#     print('KeyError')
-#
-# import pytest
-# import random
-# try:
-#     import unittest2 as unittest
-# except ImportError:
-#     import unittest
-#
-# class SimpleTest(unittest.TestCase):
-#     @unittest.skip("demonstrating skipping")
-#     def test_skipped(self):
-#         self.fail("shouldn't happen")
-#
-#     def test_pass(self):
-#         self.assertEqual(10, 7 + 3)
-#
-#     def test_fail(self):
-#         self.assertEqual(11, 7 + 3)# tests.py
-#
-# suite = unittest.TestLoader().loadTestsFromTestCase(SimpleTest)
-# unittest.TextTestRunner(verbosity=2).run(suite)
+import sys
+import os
+try:
+    print('JENKINS_WORKSPACE' + os.environ.get('JENKINS_HOME'))
+except KeyError:
+    print('KeyError')
+
+import pytest
+import random
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+class SimpleTest(unittest.TestCase):
+    @unittest.skip("demonstrating skipping")
+    def test_skipped(self):
+        self.fail("shouldn't happen")
+
+    def test_pass(self):
+        self.assertEqual(10, 7 + 3)
+
+    def test_fail(self):
+        self.assertEqual(11, 7 + 3)# tests.py
+
+suite = unittest.TestLoader().loadTestsFromTestCase(SimpleTest)
+unittest.TextTestRunner(verbosity=2).run(suite)
